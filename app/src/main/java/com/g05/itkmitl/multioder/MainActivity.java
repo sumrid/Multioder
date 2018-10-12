a5f28882502be4eb613a693772f2781983c77209
Sumrid Klinkanha <59070174@it.kmitl.ac.th>
2018-10-12 21:04:16 +0700

    Initial commit

---
+++ app/src/main/java/com/g05/itkmitl/multioder/MainActivity.java
@@ -0,0 +1,30 @@
+package com.g05.itkmitl.multioder;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if(savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.main_view, new LoginFragment())
+                    .commit();
+        }
+    }
+}
---
+++ app/src/main/java/com/g05/itkmitl/multioder/RegisterFragment.java
@@ -0,0 +1,6 @@
+package com.g05.itkmitl.multioder;
+
+import android.support.v4.app.Fragment;
+
+public class RegisterFragment extends Fragment {
+}
