2193e82e628d7670fff88ab70a16994cfeeab0cc
Oasong <59070098@it.kmitl.ac.th>
2018-10-16 21:27:41 +0700

    -- Verify Email FIX RegisterFragment ++เก็บข้อมูลในdatabase

--- app/src/main/java/com/g05/itkmitl/multioder/RegisterFragment.java
+++ app/src/main/java/com/g05/itkmitl/multioder/RegisterFragment.java
@@ -13,11 +13,14 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterFragment extends Fragment {
     @Nullable
@@ -31,23 +34,10 @@ public class RegisterFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
         initsignupBtn();
         inittoLogin();
-        inittestSignout(); // DELETE DELETE THIS (TEST)
-    }
-
-    void inittestSignout() {  // DELETE DELETE THIS (TEST)
-        TextView testSignout = getView().findViewById(R.id.testSignout);
-        testSignout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
-                firebaseAuth.signOut();
-                getFragmentManager().popBackStack();
-            }
-        });
 
     }
 
-    void inittoLogin() {
+    private void inittoLogin() {
         TextView toLogin = getView().findViewById(R.id.toLogin);
         toLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -57,7 +47,7 @@ public class RegisterFragment extends Fragment {
         });
     }
 
-    void initsignupBtn() {
+    private void initsignupBtn() {
         Button signupBtn = getView().findViewById(R.id.signupBtn);
         signupBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -66,56 +56,56 @@ public class RegisterFragment extends Fragment {
                 EditText emailReg = getView().findViewById(R.id.email_reg);
                 EditText passwordReg = getView().findViewById(R.id.passwor_reg);
                 EditText repasswordReg = getView().findViewById(R.id.repassword_reg);
+                EditText phoneReg = getView().findViewById(R.id.phone_reg);
+                EditText addressReg = getView().findViewById(R.id.address_reg);
 
                 String nameStr = nameReg.getText().toString();
                 String emailStr = emailReg.getText().toString();
                 String passwordStr = passwordReg.getText().toString();
                 String repasswordStr = repasswordReg.getText().toString();
+                String phoneStr = phoneReg.getText().toString();
+                String addressStr = addressReg.getText().toString();
 
-                if (nameStr.isEmpty() || emailStr.isEmpty() || passwordStr.isEmpty() || repasswordStr.isEmpty()){
+                if (nameStr.isEmpty() || emailStr.isEmpty() || passwordStr.isEmpty() || repasswordStr.isEmpty()
+                        || phoneStr.isEmpty() || addressStr.isEmpty()){
                     Toast.makeText(getActivity(), "Please enter your informations", Toast.LENGTH_SHORT).show();
                 }else if (!passwordStr.equals(repasswordStr)){
                     Toast.makeText(getActivity(), "Password is not match", Toast.LENGTH_SHORT).show();
                 }else{
-                    createAccount(emailStr, passwordStr);
+                    createAccount(emailStr, passwordStr, phoneStr, addressStr, nameStr);
                 }
 
             }
         });
     }
 
-    void createAccount(String user, String password) {
+    private void createAccount(String email, String password, final String phone, final String address, final String name) {
         FirebaseAuth mAuth = FirebaseAuth.getInstance();
-        mAuth.createUserWithEmailAndPassword(user, password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
-            public void onSuccess(AuthResult authResult) {
-                FirebaseUser curUser = FirebaseAuth.getInstance().getCurrentUser();
-                sendVerifiedEmail(curUser);
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Toast.makeText(getActivity(), e.getMessage(), Toast.LENGTH_SHORT).show();
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Toast.makeText(getActivity(),"Please wait...", Toast.LENGTH_LONG).show();
+                    User user = new User(name, phone, address);
+                    FirebaseDatabase.getInstance().getReference("Users")
+                            .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                            .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()){
+                                Toast.makeText(getActivity(),"Register Complete", Toast.LENGTH_LONG).show();
+                                goToLoginFragment();
+                            }else{
+                                Toast.makeText(getActivity(), task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    });
+                }
             }
         });
     }
 
-    void sendVerifiedEmail(final FirebaseUser _user) {
-        _user.sendEmailVerification().addOnSuccessListener(new OnSuccessListener < Void > () {
-            @Override public void onSuccess(Void aVoid) {
-                FirebaseAuth auth = FirebaseAuth.getInstance();
-                auth.signOut();
-                Toast.makeText(getActivity(), "Please verify your email", Toast.LENGTH_SHORT).show();
-                goToLoginFragment();
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override public void onFailure(@NonNull Exception e) {
-                Toast.makeText(getActivity(), e.getMessage(), Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
-
-    void goToLoginFragment() {
+    private void goToLoginFragment() {
         getActivity().getSupportFragmentManager()
                 .beginTransaction().replace(R.id.main_view, new LoginFragment())
                 .addToBackStack(null).commit();
